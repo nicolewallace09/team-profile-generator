4677ab655c213db217b24c2be91815bc92e0e28b
nicolewallace09 <nicole.elisaw@gmail.com>
2020-05-16 12:11:35 -0700

    added methods to pass all tests

--- index.js
+++ index.js
@@ -6,5 +6,37 @@ const Intern = require('./lib/Intern');
 
 // node modules 
 const fs = require('fs'); 
+const inquirer = require('inquirer');
 const generatePage = require
 
+// start of user prompts 
+const promptUser = () => {
+    return inquirer. prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?', 
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log ('Please enter your name!');
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is your ID?',
+            validate: nameInput => {
+                if (nameInput ) {
+                    return true;
+                } else {
+                    console.log ('Please enter a valid work ID')
+                }
+            }
+        },
+
+    ])
+}
