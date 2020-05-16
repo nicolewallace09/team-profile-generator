3356d9d22ca6d0f22b884d25704b90184e7a9b41
nicolewallace09 <nicole.elisaw@gmail.com>
2020-05-15 18:11:48 -0700

    created test to fail for engineer

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -3,7 +3,7 @@ const Engineer = require('../lib/Engineer');
 
 // creating engineer object  
 test('creates an Engineer object', () => {
-    const engineer = new Engineer('Nicole', 09, 'nicole.elisaw@gmail', 'nicolewallace09');
+    const engineer = new Engineer('Nicole', 90, 'nicole.elisaw@gmail', 'nicolewallace09');
 
     // expect(engineer.name).toEqual(expect.any(String));
     // expect(engineer.id).toEqual(expect.any(Number));
@@ -13,14 +13,14 @@ test('creates an Engineer object', () => {
 
 // gets github from getGithub()
 test('gets engineer github value', () => {
-    const engineer = new Engineer('Nicole', '09', 'nicole.elisaw@gmail', 'nicolewallace09');
+    const engineer = new Engineer('Nicole', 90, 'nicole.elisaw@gmail', 'nicolewallace09');
 
     expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()));
 });
 
 // gets role from getRole() 
 test('gets role of employee', () => {
-    const engineer = new Engineer('Nicole', '09', 'nicole.elisaw@gmail', 'nicolewallace09');
+    const engineer = new Engineer('Nicole', 90, 'nicole.elisaw@gmail', 'nicolewallace09');
 
     expect(engineer.getRole()).toEqual("Engineer");
 });
