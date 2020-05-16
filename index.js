6ec712a8cea7995927045ee8fad7ef24435df591
nicolewallace09 <nicole.elisaw@gmail.com>
2020-05-16 13:30:04 -0700

    finsished manager questions, starting employee questions

--- index.js
+++ index.js
@@ -7,9 +7,12 @@ const Intern = require('./lib/Intern');
 // node modules 
 const fs = require('fs'); 
 const inquirer = require('inquirer');
-const generatePage = require
+const generatePage = require("./src/page-template.js")
 
-// start of user prompts 
+// team array
+const teamArray = []; 
+
+// start of manager prompts 
 const addManager = () => {
     return inquirer.prompt ([
         {
@@ -20,7 +23,7 @@ const addManager = () => {
                 if (nameInput) {
                     return true;
                 } else {
-                    console.log ("Please enter a manager's name!");
+                    console.log ("Please enter the manager's name!");
                     return false; 
                 }
             }
@@ -30,10 +33,11 @@ const addManager = () => {
             name: 'id',
             message: "Please enter the manager's ID.",
             validate: nameInput => {
-                if (nameInput ) {
-                    return true;
+                if  (isNaN(nameInput)) {
+                    console.log ("Please enter the manager's ID!")
+                    return false; 
                 } else {
-                    console.log ('Please enter a valid work ID!')
+                    return true;
                 }
             }
         },
@@ -51,25 +55,95 @@ const addManager = () => {
         },
         {
             type: 'input',
-            name: 'officenumber',
-            message: "Please enter the manager's email.",
+            name: 'officeNumber',
+            message: "Please enter the manager's office number",
             validate: nameInput => {
-                if (nameInput ) {
-                    return true;
+                if  (isNaN(nameInput)) {
+                    console.log ('Please enter an office number!')
+                    return false; 
                 } else {
-                    console.log ('Please enter an email!')
+                    return true;
                 }
             }
         },
+        {
+            type: 'confirm',
+            name: 'confirmAddEmployee',
+            message: 'Would you like to add more team members?',
+            default: false
+        }
+    ])
+    .then(managerInput => {
+        const  { name, id, email, officeNumber } = managerInput; 
+        const manager = new Manager (name, id, email, officeNumber)
+
+        teamArray.push(manager); 
+
+        console.log(manager)
+    })
+};
+
+const addEmployee = () => {
+    console.log(`
+    =================
+    Adding employees to the team
+    =================
+    `);
+
+    return inquirer.prompt ([
+    {
+        type: 'input',
+        name: 'name',
+        message: "What's the name of the employee?", 
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log ("Please enter an employee's name!");
+                return false; 
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "Please enter the employee's ID.",
+        validate: nameInput => {
+            if  (isNaN(nameInput)) {
+                console.log ("Please enter the employee's ID!")
+                return false; 
+            } else {
+                return true;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "Please enter the employee's email.",
+        validate: nameInput => {
+            if (nameInput ) {
+                return true;
+            } else {
+                console.log ('Please enter an email!')
+            }
+        }
+    },
+    {
+        type: 'list',
+        name: 'role',
+        message: "Please choose employee's role.",
+        choices: ['Engineer', 'Intern']
+    }
+    ])
+};
 
 
-    ]);
-}
 
-promptUser()
-//   .then(promptProject)
-//   .then(portfolioData => {
-//     return generatePage(portfolioData);
+// addManager()
+//   .then(addEmployee)
+//   .then(teamArray => {
+//     return generatePage(teamArray);
 //   })
 //   .then(pageHTML => {
 //     return writeFile(pageHTML);
