d48d11c7b8b197b7d48c597fc7a0d09c2c3269b4
nicolewallace09 <nicole.elisaw@gmail.com>
2020-05-16 12:31:34 -0700

    installed inquirer, starting questions prompts

--- index.js
+++ index.js
@@ -10,17 +10,17 @@ const inquirer = require('inquirer');
 const generatePage = require
 
 // start of user prompts 
-const promptUser = () => {
-    return inquirer. prompt ([
+const addManager = () => {
+    return inquirer.prompt ([
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?', 
+            message: 'Who is the manager of this team?', 
             validate: nameInput => {
                 if (nameInput) {
                     return true;
                 } else {
-                    console.log ('Please enter your name!');
+                    console.log ("Please enter a manager's name!");
                     return false; 
                 }
             }
@@ -28,15 +28,52 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'id',
-            message: 'What is your ID?',
+            message: "Please enter the manager's ID.",
             validate: nameInput => {
                 if (nameInput ) {
                     return true;
                 } else {
-                    console.log ('Please enter a valid work ID')
+                    console.log ('Please enter a valid work ID!')
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Please enter the manager's email.",
+            validate: nameInput => {
+                if (nameInput ) {
+                    return true;
+                } else {
+                    console.log ('Please enter an email!')
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'officenumber',
+            message: "Please enter the manager's email.",
+            validate: nameInput => {
+                if (nameInput ) {
+                    return true;
+                } else {
+                    console.log ('Please enter an email!')
                 }
             }
         },
 
-    ])
+
+    ]);
 }
+
+promptUser()
+//   .then(promptProject)
+//   .then(portfolioData => {
+//     return generatePage(portfolioData);
+//   })
+//   .then(pageHTML => {
+//     return writeFile(pageHTML);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
